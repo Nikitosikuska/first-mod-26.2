99c586d18ce16b863e7c28f121d63ab32c912455
Nikitosikuska <nikitosikuska5@gmail.com>
2026-06-25 13:27:51 +0200

    added fluorite and raw fluorite

--- src/main/java/net/nikitosikuska/firstmod/FirstModDataGenerator.java
+++ src/main/java/net/nikitosikuska/firstmod/FirstModDataGenerator.java
@@ -2,10 +2,15 @@ package net.nikitosikuska.firstmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.nikitosikuska.firstmod.datagen.ModModelProvider;
+import org.jspecify.annotations.NonNull;
+
 //important
 public class FirstModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
